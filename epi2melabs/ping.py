b5213f3385c0d43f978c55e74b747cd5984b5cc1
Chris Wright <chris.wright@nanoporetech.com>
2020-05-26 16:51:07 +0100

    Set pings to look for host info

--- epi2melabs/ping.py
+++ epi2melabs/ping.py
@@ -1,4 +1,6 @@
 """Functions to send statistics to ONT."""
+import configparser
+import os
 import platform
 import socket
 import uuid
@@ -6,10 +8,11 @@ import uuid
 import requests
 
 
-ENDPOINT = 'https://ping.oxfordnanoportal.com/epilaby'
+ENDPOINT = 'https://ping-dev.oxfordnanoportal.com/epilaby'
+CONTAINER_META = os.path.join(os.sep, 'epi2melabs', '.epi2melabsmeta')
 
 
-def _send_ping(data, session):
+def _send_ping(data, session, hostname=None, opsys=None):
     """Attempt to send a ping to home.
 
     :param data: a dictionary containing the data to send (should be
@@ -19,16 +22,20 @@ def _send_ping(data, session):
     """
     if not isinstance(session, uuid.UUID):
         raise ValueError('`session` should be a uuid.UUID object')
-    ping_version = '1.0.0'
+    ping_version = '1.1.0'
+    if hostname is None:
+        hostname = socket.gethostname()
+    if opsys is None:
+        opsys = platform.platform()
     ping = {
         "tracking_id": {"msg_id": str(uuid.uuid4()), "version": ping_version},
-        "hostname": socket.gethostname(), "os": platform.platform(),
+        "hostname": hostname, "os": opsys,
         "session": str(session)}
     ping.update(data)
     try:
         r = requests.post(ENDPOINT, json=ping)
     except Exception as e:
-        pass
+        print(e)
     return r.status_code
 
 
@@ -41,17 +48,27 @@ class Pingu(object):
         :param session: a UUID session identifier
         """
         if session is None:
-            self.session = uuid.uuid4()
+            session = uuid.uuid4()
         else:
             if not isinstance(session, uuid.UUID):
                 raise ValueError('`session` should be a uuid.UUID object')
-            self.session = session
+        self.session = session
+        self.state = None
+        self.hostname = None
+        try:
+            config = configparser.ConfigParser()
+            config.read(CONTAINER_META)
+            self.hostname = config['Host']['hostname']
+            self.opsys = config['Host']['operating_system']
+        except Exception as e:
+            self.hostname = socket.gethostname()
+            self.opsys = platform.platform()
 
-    def send_container_ping(self, action, container, image_name, message=None):
+    def send_container_ping(self, action, container_stats, image_name, message=None):
         """Ping a status message of a container.
 
         :param action: one of 'start', 'stop', or 'update'.
-        :param container: a docker `Container` object.
+        :param container: the output of `container.stats(stream=False)`
         :param image_tag: the name of the image associated with the container.
 
         :returns: status code of HTTP request.
@@ -63,10 +80,10 @@ class Pingu(object):
         return _send_ping({
             "source": "container",
             "action": action,
-            "container_data": container.stats(stream=False),
+            "container_data": container_stats,
             "image_data": image_name,
             "message": message},
-            session=self.session)
+            session=self.session, hostname=self.hostname, opsys=self.opsys)
 
     def send_notebook_ping(self, action, notebook, message=None):
         """Ping a message from a notebook.
@@ -79,9 +96,15 @@ class Pingu(object):
         if action not in allowed_status:
             raise ValueError(
                 "`action` was not an allowed value, got: '{}'".format(action))
+        # check for spam
+        if any((
+                action == self.state,
+                action == "stop" and self.state == None)):
+            return
+        self.state = action
         return _send_ping({
             "source": "notebook",
             "action": action,
             "notebook_name": notebook,
             "message": message},
-            session=self.session)
+            session=self.session, hostname=self.hostname, opsys=self.opsys)
